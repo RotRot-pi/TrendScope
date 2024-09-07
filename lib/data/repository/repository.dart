82746484d0e0c61af8041daa173545be7a6b1c5c
RotRot-pi <ramyofficel@gmail.com>
2024-09-07 14:44:48 +0100

    Add Repository in domain layer Update RepositoryImpl in data layer

---
+++ lib/data/repository/repository.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:trendscope/data/network/app_mock_api.dart';
+import 'package:trendscope/data/network/failure.dart';
+import 'package:trendscope/domain/model/stock_data.dart';
+import 'package:trendscope/domain/repository/repository.dart';
+import 'package:trendscope/main.dart';
+
+class RepositoryImpl implements Repository {
+  final Dio _dio;
+  final AppMockApi _appMockApi;
+
+  RepositoryImpl({required Dio dio, required AppMockApi appMockApi})
+      : _dio = dio,
+        _appMockApi = appMockApi;
+  @override
+  Future<Either<Failure, StockData>> getHistoricalData(
+      String function, String symbol) async {
+    try {
+      final response = await _appMockApi.getHistoricalData(function, symbol);
+      return Right(response);
+    } catch (e) {
+      appLogger.e(e);
+      throw Exception(e.toString());
+      //     return Left(Failure(e.toString()));
+    }
+  }
+}
---
+++ lib/domain/repository/repository.dart
@@ -0,0 +1,10 @@
+//Domain Layer Repository
+
+import 'package:dartz/dartz.dart';
+import 'package:trendscope/data/network/failure.dart';
+import 'package:trendscope/domain/model/stock_data.dart';
+
+abstract class Repository {
+  Future<Either<Failure, StockData>> getHistoricalData(
+      String function, String symbol);
+}

17ef3a8b43876c53ab0119a946f447a5b41ea645
RotRot-pi <ramyofficel@gmail.com>
2024-09-07 17:10:11 +0100

    Add stock chart provider

---
+++ lib/presentation/stock_chart_view/stock_chart_provider.dart
@@ -0,0 +1,30 @@
+//Create stock chart provider using riverpod
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:trendscope/core/di.dart';
+import 'package:trendscope/data/network/failure.dart';
+import 'package:trendscope/domain/model/stock_data.dart';
+import 'package:trendscope/domain/repository/repository.dart';
+import 'package:trendscope/domain/usecases/get_historical_data.dart';
+
+//stock chart provider
+//the provider will get the data from the repository
+//i need the stock data time series for the date
+//and the daily data for the prices and volume
+
+final stockChartProvider =
+    FutureProvider.family<Either<Failure, StockData>, String>(
+  (ref, symbol) async {
+    return ref
+        .read(getHistoricalDataProvider)
+        .call('TIME_SERIES_DAILY', symbol);
+  },
+);
+final getHistoricalDataProvider = Provider<GetHistoricalData>(
+  (ref) => GetHistoricalData(
+    ref.watch(repositoryProvider),
+  ),
+);
+
+final repositoryProvider = Provider<Repository>((ref) => instance());

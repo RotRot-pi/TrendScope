5ab0289319af570f2d1918027be9abbe535ab75f
RotRot-pi <ramyofficel@gmail.com>
2024-09-07 14:09:44 +0100

    Add AppMockAPi to data layer

---
+++ lib/data/network/app_mock_api.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:trendscope/domain/model/stock_data.dart';
+
+part 'app_mock_api.g.dart';
+
+@RestApi(baseUrl: "http://127.0.0.1:5000/")
+abstract class AppMockApi {
+  factory AppMockApi(Dio dio, {String baseUrl}) = _AppMockApi;
+
+  @GET("/query")
+  Future<StockData> getHistoricalData(
+    @Query("function") String function,
+    @Query("symbol") String symbol,
+    // @Query("apikey") String apiKey,
+  );
+}

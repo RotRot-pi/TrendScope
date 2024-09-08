3e78a2a1d11dfee902bbdde595e211751faac523
RotRot-pi <ramyofficel@gmail.com>
2024-09-08 21:24:54 +0100

    Add getStocks method

--- lib/data/repository/repository.dart
+++ lib/data/repository/repository.dart
@@ -1,22 +1,31 @@
-import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:trendscope/data/network/app_mock_api.dart';
-import 'package:trendscope/data/network/failure.dart';
 import 'package:trendscope/domain/model/stock_data.dart';
+import 'package:trendscope/domain/model/stocks_response.dart';
 import 'package:trendscope/domain/repository/repository.dart';
 import 'package:trendscope/main.dart';
 
 class RepositoryImpl implements Repository {
-  final Dio _dio;
   final AppMockApi _appMockApi;
 
   RepositoryImpl({required Dio dio, required AppMockApi appMockApi})
-      : _dio = dio,
-        _appMockApi = appMockApi;
+      : _appMockApi = appMockApi;
   @override
   Future<StockData> getHistoricalData(String function, String symbol) async {
     try {
       final response = await _appMockApi.getHistoricalData(function, symbol);
+      return response;
+    } catch (e) {
+      appLogger.e(e);
+      throw Exception(e.toString());
+      //     return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<StocksResponse> getStocks() {
+    try {
+      final response = _appMockApi.getStocks();
       return response;
     } catch (e) {
       appLogger.e(e);
--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -1,9 +1,9 @@
 //Domain Layer Repository
-
-import 'package:dartz/dartz.dart';
-import 'package:trendscope/data/network/failure.dart';
 import 'package:trendscope/domain/model/stock_data.dart';
+import 'package:trendscope/domain/model/stocks_response.dart';
 
 abstract class Repository {
   Future<StockData> getHistoricalData(String function, String symbol);
+
+  Future<StocksResponse> getStocks();
 }

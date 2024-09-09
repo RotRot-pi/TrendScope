0a3114ea462df26da2a99ccadfe5aa0ce14a34a6
RotRot-pi <ramyofficel@gmail.com>
2024-09-09 19:52:48 +0100

    Add text theme Create constants file

---
+++ lib/core/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:trendscope/core/colors.dart';
+
+var textTheme = const TextTheme(
+  headlineLarge: TextStyle(
+    color: primaryColor,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  ),
+  titleMedium: TextStyle(
+    color: primaryColor,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  ),
+  titleSmall: TextStyle(
+    color: greyColor,
+    fontSize: 12,
+  ),
+);

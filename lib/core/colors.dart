43d1ff4b6381f9ef9eefee862e8d9fd97468d4b9
RotRot-pi <ramyofficel@gmail.com>
2024-09-09 19:53:15 +0100

    Add app colors

---
+++ lib/core/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFF011430);
+const Color greyColor = Color(0xFF8A8E93);
+const Color accentGreyColor = Color(0xFFE7E7E7);
+const Color whiteColor = Color(0xFFFFFFFF);

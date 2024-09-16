a02da88a4ef4d02663c27066350908c3cbda69ba
RotRot-pi <ramyofficel@gmail.com>
2024-09-16 22:38:31 +0100

    Refactor StockChart widget

--- lib/presentation/stock_chart/widgets/stock_chart_widget.dart
+++ lib/presentation/stock_chart/widgets/stock_chart_widget.dart
@@ -1,154 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:trendscope/core/constant.dart';
 import 'package:trendscope/core/enums.dart';
 import 'package:trendscope/domain/model/stock_data.dart';
-import 'package:trendscope/main.dart';
 import 'package:trendscope/presentation/common/helpers/chart_data_processor.dart';
 import 'package:trendscope/presentation/common/objects/chart_data_object.dart';
 import 'package:trendscope/presentation/stock_chart/provider/stock_chart_provider.dart';
-
 import 'package:trendscope/presentation/stock_chart/widgets/candlestick_chart_widget.dart';
 import 'package:trendscope/presentation/stock_chart/widgets/volume_chart_widget.dart';
 
 class StockChartWidget extends ConsumerWidget {
-  final String symbol;
-
-  const StockChartWidget({super.key, required this.symbol});
-
+  const StockChartWidget({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final stockDataAsyncValue = ref.watch(
-        stockChartDataProvider((symbol, ref.watch(selectedTimeFrameProvider))));
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Stock Data for $symbol"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          _buildChartTypeSelector(ref),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildTimeframeSelector(ref),
-                  _buildPeriodSelector(ref),
-                  _buildTrendlineSelector(ref),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      ref.read(showTrendlineProvider.notifier).state =
-                          !ref.read(showTrendlineProvider);
-                    },
-                    child: const Text('Toggle Trendline'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      ref.read(showOpenCloseMarkesProvider.notifier).state =
-                          !ref.read(showOpenCloseMarkesProvider);
-                    },
-                    child: const Text('Toggle Open/Closed Markers'),
-                  )
-                ],
-              ),
-            ],
-          ),
-          Expanded(
-            child: _buildChart(
-                stockDataAsyncValue,
-                ref.watch(selectedTimeFrameProvider),
-                ref.watch(selectedPeriodProvider)),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Timeframe Selector
-  Widget _buildTimeframeSelector(WidgetRef ref) {
-    return SegmentedButton<ChartTimeFrame>(
-      segments: ChartTimeFrame.values.map((timeframe) {
-        return ButtonSegment<ChartTimeFrame>(
-          value: timeframe,
-          label: Text(timeframe.toString().split('.').last),
-        );
-      }).toList(),
-      selected: {ref.watch(selectedTimeFrameProvider)},
-      onSelectionChanged: (Set<ChartTimeFrame> newSelection) {
-        final newTimeframe = newSelection.first;
-        ref.read(selectedTimeFrameProvider.notifier).state = newTimeframe;
-
-        // Adjust the selected period if necessary
-        final currentPeriod = ref.read(selectedPeriodProvider);
-        final validPeriods = _getValidPeriods(newTimeframe);
-        if (!validPeriods.contains(currentPeriod)) {
-          // Find the closest valid period
-          final newPeriod = validPeriods.lastWhere(
-            (period) =>
-                periods.indexOf(period) <= periods.indexOf(currentPeriod),
-            orElse: () => validPeriods.first,
-          );
-          ref.read(selectedPeriodProvider.notifier).state = newPeriod;
-        }
-      },
-    );
-  }
-
-  // Chart Builder
-  Widget _buildChart(AsyncValue<StockData> stockDataAsyncValue,
-      ChartTimeFrame selectedTimeFrame, String selectedPeriod) {
+    final stockDataAsyncValue =
+        ref.watch(stockChartDataProvider(ref.watch(selectedTimeFrameProvider)));
+    final selectedTimeFrame = ref.watch(selectedTimeFrameProvider);
+    final selectedPeriod = ref.watch(selectedPeriodProvider);
     return stockDataAsyncValue.when(
       data: (stockData) {
         final chartData = _processChartData(stockData, selectedTimeFrame);
+
+        // If there's no data, show a placeholder message
         if (chartData.isEmpty) {
           return const Center(
-              child: Text('No data available for this timeframe.'));
+            child: Text('No data available for this timeframe.'),
+          );
         }
+
+        // Calculate chart configuration based on processed chart data
         final config = ChartDataProcessor.calculateChartConfig(
           chartData,
           selectedTimeFrame,
           selectedPeriod,
         );
+
         return Column(
           children: [
+            // CandleStick Chart takes the majority of the space
             Expanded(
               child: CandleStickChartWidget(
-                  minimumDate: config.minimumDate,
-                  maximumDate: config.maximumDate,
-                  minPrice: config.minPrice,
-                  maxPrice: config.maxPrice,
-                  dateFormat: config.dateFormat,
-                  chartData: chartData),
+                minimumDate: config.minimumDate,
+                maximumDate: config.maximumDate,
+                minPrice: config.minPrice,
+                maxPrice: config.maxPrice,
+                dateFormat: config.dateFormat,
+                chartData: chartData,
+              ),
             ),
+            // Volume chart widget, occupying 100px height
             SizedBox(
               height: 100,
               child: VolumeChartWidget(
-                  minimumDate: config.minimumDate,
-                  maximumDate: config.maximumDate,
-                  dateFormat: config.dateFormat,
-                  chartData: chartData),
+                minimumDate: config.minimumDate,
+                maximumDate: config.maximumDate,
+                dateFormat: config.dateFormat,
+                chartData: chartData,
+              ),
             ),
           ],
         );
       },
+      // Show loading spinner while data is being fetched
       loading: () => const Center(child: CircularProgressIndicator()),
-      error: (error, stackTrace) =>
-          Center(child: Text('Error loading data: $error')),
+      // Display an error message if something goes wrong
+      error: (error, stackTrace) => Center(
+        child: Text('Error loading data: $error'),
+      ),
     );
   }
 
+  // Function to process raw stock data into chart data
   List<ChartData> _processChartData(
       StockData stockData, ChartTimeFrame timeFrame) {
+    // Switch between different time series based on the selected timeframe
     Map<String, dynamic>? timeSeries;
     switch (timeFrame) {
       case ChartTimeFrame.daily:
@@ -168,16 +93,19 @@ class StockChartWidget extends ConsumerWidget {
         break;
     }
 
+    // If there's no data available for the selected timeframe, return an empty list
     if (timeSeries == null || timeSeries.isEmpty) {
-      appLogger.w("No data available for $timeFrame");
       return [];
     }
 
+    // Sort the time series entries by date in descending order
     var sortedEntries = timeSeries.entries.toList()
       ..sort((a, b) => b.key.compareTo(a.key));
 
-    if (sortedEntries.isEmpty) return []; // Handle empty data
+    // If sorted entries are empty, return an empty list
+    if (sortedEntries.isEmpty) return [];
 
+    // Map each entry to ChartData, parsing the date and other values
     return sortedEntries.map((entry) {
       final date = DateFormat('yyyy-MM-dd').parse(entry.key);
       final data = entry.value;
@@ -190,112 +118,5 @@ class StockChartWidget extends ConsumerWidget {
         volume: double.tryParse(data.volume) ?? 0,
       );
     }).toList();
-  }
-
-  Widget _buildPeriodSelector(WidgetRef ref) {
-    final selectedTimeFrame = ref.watch(selectedTimeFrameProvider);
-    final validPeriods = _getValidPeriods(selectedTimeFrame);
-    appLogger.i("Valid periods: $validPeriods");
-    return DropdownButton<String>(
-      value: ref.watch(selectedPeriodProvider),
-      items: validPeriods.map((period) {
-        appLogger.i("Period: $period");
-        return DropdownMenuItem<String>(
-          value: period,
-          child: Text(period),
-        );
-      }).toList(),
-      onChanged: (String? newPeriod) {
-        ref.read(selectedPeriodProvider.notifier).state = newPeriod!;
-      },
-    );
-  }
-
-  List<String> _getValidPeriods(ChartTimeFrame timeframe) {
-    switch (timeframe) {
-      case ChartTimeFrame.daily:
-        return [
-          'Last Day',
-          'Last Week',
-          'Last Month',
-          'Last 3 Months',
-          'Last 6 Months',
-          'Last Year',
-          '5 Years'
-        ];
-      case ChartTimeFrame.weekly:
-        return [
-          'Last Week',
-          'Last Month',
-          'Last 3 Months',
-          'Last 6 Months',
-          'Last Year',
-          '5 Years'
-        ];
-      case ChartTimeFrame.monthly:
-        return [
-          'Last Month',
-          'Last 3 Months',
-          'Last 6 Months',
-          'Last Year',
-          '5 Years'
-        ];
-      case ChartTimeFrame.yearly:
-        return ['Last Year', '5 Years'];
-      case ChartTimeFrame.fiveYears:
-        return ['5 Years'];
-      default:
-        return [
-          'Last Day',
-          'Last Week',
-          'Last Month',
-          'Last 3 Months',
-          'Last 6 Months',
-          'Last Year',
-          '5 Years'
-        ];
-    }
-  }
-
-  // Trendline Selection UI
-  Widget _buildTrendlineSelector(WidgetRef ref) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _buildTrendlineCheckbox(ref, 'Linear', TrendlineType.linear),
-        _buildTrendlineCheckbox(
-            ref, 'Moving Average', TrendlineType.movingAverage),
-        _buildTrendlineCheckbox(ref, 'Exponential', TrendlineType.exponential),
-      ],
-    );
-  }
-
-  // Helper function to create a checkbox for each trendline type
-  Widget _buildTrendlineCheckbox(
-      WidgetRef ref, String label, TrendlineType type) {
-    return Row(
-      children: [
-        Checkbox(
-          value: ref.watch(trendlineVisibilityProvider(type)),
-          onChanged: (value) {
-            ref.read(trendlineVisibilityProvider(type).notifier).state = value!;
-          },
-        ),
-        Text(label),
-      ],
-    );
-  }
-
-  Widget _buildChartTypeSelector(WidgetRef ref) {
-    return SegmentedButton<String>(
-      segments: const [
-        ButtonSegment(value: 'Candlestick', label: Text('Candlestick')),
-        ButtonSegment(value: 'Line', label: Text('Line')),
-      ],
-      selected: {ref.watch(selectedChartTypeProvider)},
-      onSelectionChanged: (Set<String> newSelection) {
-        ref.read(selectedChartTypeProvider.notifier).state = newSelection.first;
-      },
-    );
   }
 }

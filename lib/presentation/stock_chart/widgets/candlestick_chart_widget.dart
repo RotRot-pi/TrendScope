0beb97e29f5d8c71059bcc177c288e1f3c07ea63
RotRot-pi <ramyofficel@gmail.com>
2024-09-18 21:40:55 +0100

    Add volume chart to the candlstick chart in the same chart Minor refactor

--- lib/presentation/stock_chart/widgets/candlestick_chart_widget.dart
+++ lib/presentation/stock_chart/widgets/candlestick_chart_widget.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
@@ -13,118 +14,141 @@ class CandleStickChartWidget extends ConsumerWidget {
     required this.maximumDate,
     required this.minPrice,
     required this.maxPrice,
-    required this.chartData,
+    required this.maxVolume,
+    required this.minVolume,
     required this.dateFormat,
+    required this.chartData,
   });
 
   final DateTime minimumDate;
   final DateTime maximumDate;
   final double? minPrice;
   final double? maxPrice;
+  final double? maxVolume;
+  final double? minVolume;
   final DateFormat dateFormat;
   final List<ChartData> chartData;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedChartType = ref.watch(selectedChartTypeProvider);
-    return Column(
-      children: [
-        SfCartesianChart(
-          legend: const Legend(isVisible: true, position: LegendPosition.top),
-          indicators: [
-            RsiIndicator<ChartData, DateTime>(
-              seriesName: 'Candlestick',
-              period: 14,
-              dataSource: chartData,
-              xValueMapper: (ChartData data, _) => data.date,
-              lowValueMapper: (ChartData data, _) => data.low,
-              highValueMapper: (ChartData data, _) => data.high,
-              closeValueMapper: (ChartData data, _) => data.close,
-              signalLineColor: Colors.red,
-            ),
-            SmaIndicator<ChartData, DateTime>(
-              seriesName: 'Candlestick',
-              period: 5,
-              dataSource: chartData,
-              xValueMapper: (ChartData data, _) => data.date,
-              lowValueMapper: (ChartData data, _) => data.low,
-              highValueMapper: (ChartData data, _) => data.high,
-              openValueMapper: (ChartData data, _) => data.open,
-              closeValueMapper: (ChartData data, _) => data.close,
-              signalLineColor: Colors.green,
-            ),
-            SmaIndicator<ChartData, DateTime>(
-              seriesName: 'Candlestick',
-              period: 20,
-              dataSource: chartData,
-              xValueMapper: (ChartData data, _) => data.date,
-              lowValueMapper: (ChartData data, _) => data.low,
-              highValueMapper: (ChartData data, _) => data.high,
-              openValueMapper: (ChartData data, _) => data.open,
-              closeValueMapper: (ChartData data, _) => data.close,
-              signalLineColor: Colors.amber,
-            ),
-            EmaIndicator<ChartData, DateTime>(
-                dataSource: chartData,
-                xValueMapper: (ChartData data, _) => data.date,
-                lowValueMapper: (ChartData data, _) => data.low,
-                highValueMapper: (ChartData data, _) => data.high,
-                closeValueMapper: (ChartData data, _) => data.close,
-                signalLineColor: Colors.blue,
-                seriesName: 'Candlestick')
-          ],
-          zoomPanBehavior: ZoomPanBehavior(
-              enableSelectionZooming: true,
-              enablePinching: true,
-              enableDoubleTapZooming: true,
-              enableMouseWheelZooming: true,
-              enablePanning: true,
-              zoomMode: ZoomMode.xy),
-          primaryXAxis: DateTimeAxis(
-            minimum: minimumDate,
-            maximum: maximumDate,
-            dateFormat: dateFormat,
-            intervalType: DateTimeIntervalType.days,
+    final symbol = ref.watch(selectedSymbolProvider);
+    return SizedBox(
+      height: 300,
+      child: SfCartesianChart(
+        axes: [
+          NumericAxis(
+            isVisible: false,
+            name: 'Volume Y-Axis',
+            minimum: minVolume,
+            maximum: maxVolume,
             majorGridLines:
                 const MajorGridLines(width: 0.5, color: Colors.grey),
           ),
-          primaryYAxis: NumericAxis(
-            minimum: minPrice,
-            maximum: maxPrice,
-            numberFormat:
-                NumberFormat.compactCurrency(symbol: '\$', decimalDigits: 0),
-            decimalPlaces: 2,
-            majorGridLines:
-                const MajorGridLines(width: 0.5, color: Colors.grey),
-          ),
-          series: [
-            if (selectedChartType == 'Candlestick')
-              CandleSeries<ChartData, DateTime>(
-                dataSource: chartData,
-                xValueMapper: (ChartData data, _) => data.date,
-                lowValueMapper: (ChartData data, _) => data.low,
-                highValueMapper: (ChartData data, _) => data.high,
-                openValueMapper: (ChartData data, _) => data.open,
-                closeValueMapper: (ChartData data, _) => data.close,
-                enableTooltip: true,
-                trendlines: _getTrendlines(ref),
-                name: 'Candlestick',
-              ),
-            if (selectedChartType == 'Line')
-              LineSeries<ChartData, DateTime>(
-                dataSource: chartData,
-                xValueMapper: (ChartData data, _) => data.date,
-                yValueMapper: (ChartData data, _) => data.close,
-              ),
-            if (ref.watch(showOpenCloseMarkesProvider))
-              _buildScatterForOpeningPrice(),
-            if (ref.watch(showOpenCloseMarkesProvider))
-              _buildScatterForClosingPrice(),
-          ],
-          tooltipBehavior: TooltipBehavior(enable: true),
-          crosshairBehavior: CrosshairBehavior(enable: true),
+        ],
+        legend: const Legend(
+          isVisible: true,
+          position: LegendPosition.top,
         ),
-      ],
+        indicators: [
+          RsiIndicator<ChartData, DateTime>(
+            seriesName: symbol,
+            period: 14,
+            dataSource: chartData,
+            xValueMapper: (ChartData data, _) => data.date,
+            lowValueMapper: (ChartData data, _) => data.low,
+            highValueMapper: (ChartData data, _) => data.high,
+            closeValueMapper: (ChartData data, _) => data.close,
+            signalLineColor: Colors.red,
+          ),
+          SmaIndicator<ChartData, DateTime>(
+            seriesName: symbol,
+            period: 5,
+            dataSource: chartData,
+            xValueMapper: (ChartData data, _) => data.date,
+            lowValueMapper: (ChartData data, _) => data.low,
+            highValueMapper: (ChartData data, _) => data.high,
+            openValueMapper: (ChartData data, _) => data.open,
+            closeValueMapper: (ChartData data, _) => data.close,
+            signalLineColor: Colors.green,
+          ),
+          SmaIndicator<ChartData, DateTime>(
+            seriesName: symbol,
+            period: 20,
+            dataSource: chartData,
+            xValueMapper: (ChartData data, _) => data.date,
+            lowValueMapper: (ChartData data, _) => data.low,
+            highValueMapper: (ChartData data, _) => data.high,
+            openValueMapper: (ChartData data, _) => data.open,
+            closeValueMapper: (ChartData data, _) => data.close,
+            signalLineColor: Colors.amber,
+          ),
+          EmaIndicator<ChartData, DateTime>(
+              dataSource: chartData,
+              xValueMapper: (ChartData data, _) => data.date,
+              lowValueMapper: (ChartData data, _) => data.low,
+              highValueMapper: (ChartData data, _) => data.high,
+              closeValueMapper: (ChartData data, _) => data.close,
+              signalLineColor: Colors.blue,
+              seriesName: symbol)
+        ],
+        zoomPanBehavior: ZoomPanBehavior(
+            enableSelectionZooming: true,
+            enablePinching: true,
+            enableDoubleTapZooming: true,
+            enableMouseWheelZooming: true,
+            enablePanning: true,
+            zoomMode: ZoomMode.xy),
+        primaryXAxis: DateTimeAxis(
+          minimum: minimumDate,
+          maximum: maximumDate,
+          dateFormat: dateFormat,
+          intervalType: DateTimeIntervalType.days,
+          majorGridLines: const MajorGridLines(width: 0.5, color: Colors.grey),
+        ),
+        primaryYAxis: NumericAxis(
+          minimum: minPrice,
+          maximum: maxPrice,
+          numberFormat:
+              NumberFormat.compactCurrency(symbol: '\$', decimalDigits: 0),
+          decimalPlaces: 2,
+          majorGridLines: const MajorGridLines(width: 0.5, color: Colors.grey),
+        ),
+        series: [
+          if (selectedChartType == 'Candlestick')
+            CandleSeries<ChartData, DateTime>(
+              dataSource: chartData,
+              xValueMapper: (ChartData data, _) => data.date,
+              lowValueMapper: (ChartData data, _) => data.low,
+              highValueMapper: (ChartData data, _) => data.high,
+              openValueMapper: (ChartData data, _) => data.open,
+              closeValueMapper: (ChartData data, _) => data.close,
+              enableTooltip: true,
+              trendlines: _getTrendlines(ref),
+              name: symbol,
+            ),
+          if (selectedChartType == 'Line')
+            LineSeries<ChartData, DateTime>(
+              dataSource: chartData,
+              xValueMapper: (ChartData data, _) => data.date,
+              yValueMapper: (ChartData data, _) => data.close,
+            ),
+          if (ref.watch(showOpenCloseMarkesProvider))
+            _buildScatterForOpeningPrice(),
+          if (ref.watch(showOpenCloseMarkesProvider))
+            _buildScatterForClosingPrice(),
+          ColumnSeries<ChartData, DateTime>(
+            dataSource: chartData,
+            name: 'Volume',
+            yAxisName: 'Volume Y-Axis',
+            xValueMapper: (ChartData data, _) => data.date,
+            yValueMapper: (ChartData data, _) => data.volume,
+            enableTooltip: true,
+          ),
+        ],
+        tooltipBehavior: TooltipBehavior(enable: true),
+        crosshairBehavior: CrosshairBehavior(enable: true),
+      ),
     );
   }
 

be39cbdf8a918e26581a2f6f7a8346deb280b932
RotRot-pi <ramyofficel@gmail.com>
2024-09-18 21:41:21 +0100

    Minor refactor

--- lib/presentation/stock_chart/widgets/period_selector.dart
+++ lib/presentation/stock_chart/widgets/period_selector.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:trendscope/core/colors.dart';
 import 'package:trendscope/presentation/common/helpers/stock_chart_helper.dart';
 import 'package:trendscope/presentation/stock_chart/provider/stock_chart_provider.dart';
 
@@ -14,44 +15,42 @@ class PeriodSelector extends ConsumerWidget {
     // Get valid periods based on the selected time frame
     final validPeriods = StockChartHelpers.getValidPeriods(selectedTimeFrame);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: 16, vertical: 8), // mx-4 mb-4 in Tailwind
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.white,
-          padding: const EdgeInsets.all(12), // Equivalent to p-2
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10), // rounded-lg
-          ),
-          elevation: 5, // Shadow effect
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        padding: const EdgeInsets.all(12), // Equivalent to p-2
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: const BorderSide(color: primaryColor),
         ),
-        onPressed: () async {
-          // Show a dropdown menu when the button is pressed
-          final selected = await showMenu<String>(
-            context: context,
-            position: const RelativeRect.fromLTRB(100, 200, 100, 100),
-            items: validPeriods.map((String period) {
-              return PopupMenuItem<String>(
-                value: period,
-                child: Text(period),
-              );
-            }).toList(),
-          );
+        elevation: 0, // Shadow effect
+      ),
+      onPressed: () async {
+        // Show a dropdown menu when the button is pressed
+        final selected = await showMenu<String>(
+          context: context,
+          color: Colors.white,
+          position: const RelativeRect.fromLTRB(100, 200, 100, 100),
+          items: validPeriods.map((String period) {
+            return PopupMenuItem<String>(
+              value: period,
+              child: Text(period),
+            );
+          }).toList(),
+        );
 
-          // If a period was selected, update the state
-          if (selected != null && selected != selectedPeriod) {
-            ref.read(selectedPeriodProvider.notifier).state = selected;
-          }
-        },
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(selectedPeriod, style: const TextStyle(color: Colors.black)),
-            Icon(Icons.expand_more,
-                color: Colors.grey[600]), // ChevronDown equivalent
-          ],
-        ),
+        // If a period was selected, update the state
+        if (selected != null && selected != selectedPeriod) {
+          ref.read(selectedPeriodProvider.notifier).state = selected;
+        }
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(selectedPeriod, style: const TextStyle(color: Colors.black)),
+          Icon(Icons.expand_more,
+              color: Colors.grey[600]), // ChevronDown equivalent
+        ],
       ),
     );
   }

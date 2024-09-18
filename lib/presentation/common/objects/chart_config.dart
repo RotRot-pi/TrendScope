442ba5a285e626c029951af0131090f7b2aee442
RotRot-pi <ramyofficel@gmail.com>
2024-09-18 21:37:14 +0100

    Add volume max & min fields

--- lib/presentation/common/objects/chart_config.dart
+++ lib/presentation/common/objects/chart_config.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:intl/intl.dart';
 
 class ChartConfig {
@@ -5,6 +6,8 @@ class ChartConfig {
   final DateTime maximumDate;
   final double minPrice;
   final double maxPrice;
+  final double maxVolume;
+  final double minVolume;
   final DateFormat dateFormat;
 
   ChartConfig({
@@ -12,6 +15,8 @@ class ChartConfig {
     required this.maximumDate,
     required this.minPrice,
     required this.maxPrice,
+    required this.maxVolume,
+    required this.minVolume,
     required this.dateFormat,
   });
 }

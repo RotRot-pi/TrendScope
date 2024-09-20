761ac0e734d183158bfd9917dfedc3faed3bc328
RotRot-pi <ramyofficel@gmail.com>
2024-09-20 19:40:56 +0100

    Add getTopMoversData

--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -1,6 +1,7 @@
 //Domain Layer Repository
 import 'package:trendscope/domain/model/stock_data.dart';
 import 'package:trendscope/domain/model/stocks_response.dart';
+import 'package:trendscope/domain/model/top_movers_data.dart';
 
 abstract class Repository {
   Future<StockData> getHistoricalDataDaily(String symbol);
@@ -12,6 +13,8 @@ abstract class Repository {
   Future<StockData> getHistoricalDataYearly(String symbol);
 
   Future<StockData> getHistoricalDataFiveYears(String symbol);
+
+  Future<TopMoversData> getTopMoversData();
 
   Future<StocksResponse> getStocks();
 }
